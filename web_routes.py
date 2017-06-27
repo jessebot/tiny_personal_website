a902078fb9526ed1e66ae0235ce3eef45c2acbe8
jessebot <jessebot@linux.com>
2017-06-27 06:55:43 +0000

    updating profile blurb and fonts...

--- web_routes.py
+++ web_routes.py
@@ -6,24 +6,25 @@
 import bottle
 from bottle import redirect, request, response, route
 from bottle import run, static_file, template
-import logging
+import logging as log
+import os
 import sys
 import yaml
 
 
 def get_global_variable(global_variable):
     """ gets global variable given string variable name"""
-    with open('./.config/config.yaml', 'r') as f:
+    with open('./config/config.yaml', 'r') as f:
         doc = yaml.load(f)
     txt = doc["Globals"][global_variable]
     return txt
 
 
+# set logging
+log.basicConfig(stream=sys.stderr, level=log.INFO)
+log.info("logging config loaded")
 # Grab site specific information
 WEB_ROOT = get_global_variable('web_root')
-# set logging
-logging.basicConfig(stream=sys.stderr, level=logging.INFO)
-logging.info("logging config loaded")
 # full path to HTML templates
 bottle.TEMPLATE_PATH.insert(0,
                             '{0}/front_end/html_templates/'.format(WEB_ROOT))
@@ -36,6 +37,7 @@ def index():
     main_pic = get_global_variable('profile_image')
     name = get_global_variable('profile_name')
     header_quotation = get_global_variable('profile_quote')
+    blurb = get_global_variable('profile_blurb')
     GitHub_URL = get_global_variable('github_URL')
     gdoc_URL = get_global_variable('resume_google_doc')
     resume_pdf_download_URL = get_global_variable('resume_pdf_download')
@@ -57,7 +59,7 @@ def index():
                     resume_docx_download_URL=resume_docx_download_URL,
                     fork_me=fork_me, optional_panel=optional_panel,
                     optional_panel_title=optional_panel_title,
-                    optional_panel_pic=optional_panel_pic,
+                    optional_panel_pic=optional_panel_pic, blurb=blurb,
                     optional_panel_button_text=optional_panel_button_text,
                     optional_panel_button_URL=optional_panel_button_URL)
 
@@ -76,8 +78,6 @@ def js(filename):
 def css(filename):
     return static_file(filename, root='{0}/front_end/css'.format(WEB_ROOT))
 
-
-# example of how to create another route to something else
-@route('/hate')
-def hate():
-    return template('hate')
+@route('/fonts/<filename>')
+def fonts(filename):
+    return static_file(filename, root='{0}/front_end/fonts'.format(WEB_ROOT))
