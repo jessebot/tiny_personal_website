ce7b869b086803ebfddf0adecad69d92fb00e207
jessebot <jessebot@linux.com>
2019-01-11 11:05:45 +0000

    removing python3 code

--- band_names_db.py
+++ band_names_db.py
@@ -3,23 +3,8 @@
 # 1/7/19 -- 2019? D:
 import argparse
 import datetime
-import random
 import sqlite3
-from google_images_download import google_images_download
 
-
-def generate_band_art(band):
-    # change image results by making different key words
-    band_list = band.split()
-    last_item = len(band_list) - 1
-    keywd = band_list[last_item]
-    # google image
-    response = google_images_download.googleimagesdownload()
-    absolute_img_paths = response.download({"keywords":keywd, "size":"medium",
-                                            "output_directory":"./images/band",
-                                            "no_directory":"true", "limit":1})
-    print(type(absolute_img_paths))
-    print(absolute_img_paths)
 
 def add_new_band(band):
     """
